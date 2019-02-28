3f3218668c1ddc8eee247eddaa47268fe5112bce
Joseph Fahnestock <joeyfahnestock@gmail.com>
2019-02-28 12:31:21 -0600

    IO script now functions

--- input_output/input_script.py
+++ input_output/input_script.py
@@ -1,5 +1,28 @@
-file_name = "null"
+def get_vh(file_name):
+    """Returns vertical, horizontal lists of [id, [tags]]"""
+    file = open(file_name, "r")
 
-file = open(file_name, "r")
+    lines = file.read().split("\n")
 
-print(file.read())
+    horizontal = []
+
+    vertical = []
+
+    id = 0
+    
+    print(lines[1:])
+
+    for line in lines[1:-1]:
+        new_element = []
+        if line[0] == "H":
+            new_element.append(id)
+            new_element.append(line.split()[2:])
+            horizontal.append(new_element)
+        else:
+            new_element.append(id)
+            new_element.append(line.split()[2:])
+            vertical.append(new_element)
+        id += 1
+
+
+
