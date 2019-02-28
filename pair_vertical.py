963de187d0c5a224dae94b4de1e902fe9c696cbd
irlbeck2 <irlbeck2@illinois.edu>
2019-02-28 13:45:43 -0600

    Committing changes to pair_vertical

--- pair_vertical.py
+++ pair_vertical.py
@@ -1,32 +1,31 @@
 import random
 from input_output.input_script import get_vh
 from slide import Image
+from slide import Slide
 
-images = []
-
-file_names = ["a_example.txt", "b_lovely_landscapes", "c_memorable_moments.txt", "d_pet_pictures.txt", "e_shiny_selfies"]
-
-for file_name in file_names:
-    vertical, horizontal = get_vh("input_output/" + file_name)
+def pair_vertical(input_file):
+    slides = []
+    vertical, horizontal = get_vh("input_output/" + input_file)
     while (len(vertical) > 0):
         rand_int = random.randint(1, len(vertical))
-        ids = (vertical[0][0], vertical[rand_int][0])
-        tags = merge(vertical[0][1], vertical[rand_int][1])
-        images.append(Image(ids, False, tags))
+        Image1 = Image(vertical[0][0], False, vertical[0][1])
+        Image2 = Image(vertical[rand_int][0], False, vertical[rand_int][1])
+        Slide = Slide(Image1, Image2)
+        slides.append(Slide)
         vertical.pop(0)
         vertical.pop(rand_int - 1)
 
     for i in range(len(horizontal)):
-        id = horizontal[i][0]
-        tags = horizontal[i][1]
-        images.append(id, True, tags)
+        Image_horizontal = Image(horizontal[i][0], True, horizontal[i][1])
+        Slide_h = Slide(Image_horizontal)
+    slides.append(Slide_h)
 
-    def merge(tags_1, tags_2): ## merging the tags of two vertical photos
-        tag_set = {}
-        for i in range(len(tags_1)):
-            tag_set.add(tags_1[i])
+def merge(tags_1, tags_2): ## merging the tags of two vertical photos
+    tag_set = {}
+    for i in range(len(tags_1)):
+        tag_set.add(tags_1[i])
 
-        for i in range(len(tags_2)):
-            tag_set.add(tags_2[i])
+    for i in range(len(tags_2)):
+        tag_set.add(tags_2[i])
 
-        return list(tag_set)
+    return list(tag_set)
